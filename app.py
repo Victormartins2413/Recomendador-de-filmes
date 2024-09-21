23825593ba6a6d376229846cdec7584e6ed5866b
Victormartins2413 <132926388+Victormartins2413@users.noreply.github.com>
2024-09-20 22:03:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-# Tente carregar o CSV
+# Ajuste o caminho conforme necessário
 try:
-    df = pd.read_csv("data/all_movies.csv", delimiter=';')  # Ajuste o delimitador conforme o formato real do CSV
+    df = pd.read_csv("all_movies.csv", delimiter=';')  # Ou use "data/all_movies.csv" se estiver em uma pasta chamada data
 except Exception as e:
     st.error(f"Erro ao ler o arquivo CSV: {e}")
+    df = pd.DataFrame()  # Inicializa um DataFrame vazio para evitar NameError
 
 # Função para exibir as informações do filme
 def show_movie_info(title):
@@ -18,12 +19,11 @@ def show_movie_info(title):
 # Configuração do título do webapp
 st.title("Sistema de Recomendação de Filmes")
 
-# Campo de seleção de filmes
-if 'title_pt' in df.columns:
+if not df.empty and 'title_pt' in df.columns:
     movie_choice = st.selectbox("Escolha um filme", df['title_pt'])
 
     # Exibir informações quando um filme for escolhido
     if st.button("Filtrar"):
         show_movie_info(movie_choice)
 else:
-    st.error("Coluna 'title_pt' não encontrada no DataFrame.")
+    st.error("Coluna 'title_pt' não encontrada no DataFrame ou o DataFrame está vazio.")
